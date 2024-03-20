c1ee71d1cff281115bafcb167ed880565d3b9fce
Don Joseph Lachica <44009377+Zerotsuu@users.noreply.github.com>
2024-03-20 14:54:10 +0800

    Fixed Clickable Cards while checking cards

--- src/stackedDeck.js
+++ src/stackedDeck.js
@@ -6,27 +6,23 @@ import CardGrid from "./CardGrid";
 
 export default function stackedDeck() {
   const [clickedImages, setClickedImages] = useState([]);
-  const [checkCards, setCheckCards] = useState(false)
+  const [checkCards, setCheckCards] = useState(false);
 
   const clickedCard = (index) => {
-
     if (!clickedImages.includes(index)) {
       setClickedImages((prevClickedImages) => [...prevClickedImages, index]);
     }
-  
   };
-  console.log(checkCards)
+  console.log(checkCards);
   const handleCheckCards = () => {
-  
     const numCards = clickedImages.length;
-    console.log(numCards)
-    setCheckCards(true)
-    
+    console.log(numCards);
+    setCheckCards(true);
   };
-  const resetCards = () =>{
-    setClickedImages([])
-    setCheckCards(false)
-  }
+  const resetCards = () => {
+    setClickedImages([]);
+    setCheckCards(false);
+  };
 
   return (
     <>
@@ -41,11 +37,19 @@ export default function stackedDeck() {
             </h1>
           </div>
           {/* Images */}
-          <div className="flex  justify-between ">
+          <div
+            className={`flex justify-between ${
+              checkCards ? "pointer-events-none" : ""
+            }`}
+          >
             {Array.from({ length: 20 }, (_, i) => (
               <div
                 key={i}
-                className={`filter h-80 rounded-2xl flex bg-black cursor-pointer  hover:mt-10 ${i !== 0 ? "-ml-52 " : ""} ${clickedImages.includes(i)?"mt-10":""} ${clickedImages.includes(i)?"grayscale":""}`}
+                className={`filter h-80 rounded-2xl flex bg-black cursor-pointer  hover:mt-10 ${
+                  i !== 0 ? "-ml-52 " : ""
+                } ${clickedImages.includes(i) ? "mt-10" : ""} ${
+                  clickedImages.includes(i) ? "grayscale" : ""
+                }`}
               >
                 <img
                   src={cardBack}
@@ -62,19 +66,20 @@ export default function stackedDeck() {
           <button
             type="button"
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-            onClick={handleCheckCards}>
+            onClick={handleCheckCards}
+          >
             Check your cards
           </button>
           <button
             type="button"
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-            onClick={resetCards}>
+            onClick={resetCards}
+          >
             Reset Cards
           </button>
         </div>
-        
       </div>
-      {checkCards && <CardGrid numCards={clickedImages.length}/>}
+      {checkCards && <CardGrid numCards={clickedImages.length} />}
     </>
   );
 }
